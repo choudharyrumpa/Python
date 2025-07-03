2a264f28e450dfd60cc7bef1d28de0894d03808b
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-07-03 16:10:53 +0530

    Add files via upload

---
+++ 04_operators.py
@@ -0,0 +1,42 @@
+#Arithmatic Operators
+a=7
+b=4
+c=a+b
+print (c)
+
+#Assignment Operators
+a=4-2 # Assign 4-2 in a
+print(a)
+b=6
+b+=3 #Increment the value of b by 3 and assign it to b
+print(b)
+b-=3 # Dcrement the value of by 3 and assign it to b
+print(b)
+
+#Comparison Operators
+d=5<4
+print(d)
+e=5>4
+print (e)
+f=5!=7
+print (f)
+# Logical Operators
+
+# Truth table of 'or'
+print("True or False",True or False)
+print ("True or True",True or True)
+print("False or True",False or True)
+print("False or False",False or False)
+
+# Truth table of 'and'
+print("True and False",True and False)
+print ("True and True",True and True)
+print("False and True",False and True)
+print("False and False",False and False)
+
+# not Operator
+print(not(True))
+print(not(False))
+
+
+
