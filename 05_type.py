3164ac0f2c9c73e8f1c76212f88a2e8d5e2d3c0a
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-07-04 17:21:57 +0530

    Add files via upload

---
+++ 05_type.py
@@ -0,0 +1,18 @@
+a=34
+t=type(a) # class <int>
+print(t)
+b=45.45
+s=type(b) # class <float>
+print(s)
+c="Harry"
+r=type(c)
+print (r)
+d="456.7"
+u=type(d)
+print (u)
+a="31.32"
+b=float(a) #a but the type should be float
+t=type(a)
+print (t)
+t=type(b)
+print (t)
