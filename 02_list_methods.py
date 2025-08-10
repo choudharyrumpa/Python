49c22d4b7a6a0d8e7dc5ee2468cfac9809f9e39a
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-08-10 22:51:00 +0530

    Add files via upload

---
+++ 02_list_methods.py
@@ -0,0 +1,16 @@
+friends = ["Apple" ,"Orange" ,5, 345.6 ,False,"Aakash","Rohan"]
+print (friends)
+friends.append("Harry")
+print (friends)
+
+l1 =[1 ,34,62,2,6,11]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+l1.insert(3,3333)  #Insert 3333 such that its index in the list is 3
+print(l1)
+l1.pop(3)
+print(l1)
+l1.remove(11)
+print(l1)
