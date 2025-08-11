c4b482f22101e2fd7225afde448cf2a023ab5a56
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-08-11 23:00:14 +0530

    Add files via upload

---
+++ 03_tuple.py
@@ -0,0 +1,9 @@
+a=(1,2,5,6 ,"False","Harry")
+print(type(a))
+b=()
+print(type(b))
+c=(1)
+print(type(c)) # if one digit consider as tuple than python does not understand
+# so write it like
+d=(1,)
+print(type(d)) # tuple are immutable so we cannot change the value
