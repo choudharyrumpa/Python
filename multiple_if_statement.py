3a6e7e55aa7228b3a72c8807ff611fdc3500c8dc
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-08-27 14:25:38 +0530

    Add files via upload

---
+++ multiple_if_statement.py
@@ -0,0 +1,18 @@
+a= int(input("Enter your age : " ))
+# if statement no : 1
+if(a%2 == 0):
+    print("a is even")
+#  End of nif statement no : 1
+
+# if statement no :2
+if(a>=18):
+    print("You are above the age of consent")
+    print("Good for you")
+
+elif(a<0):
+    print("You are Entering an invalid age")
+
+else:
+    print("You are below the age of consent") 
+#  End ofif statement no :2
+print("End of program") 
