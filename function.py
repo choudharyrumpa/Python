1541e25f53197b30655faf89a6f9fb2a62f50782
choudharyrumpa <choudharyrumpa36@gmail.com>
2025-09-05 20:14:04 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,15 @@
+# a=32
+# b=34
+# c=67
+# avg=(a+b+c)/3
+# print(avg)
+def avg():
+    a=int(input("Enter your number: "))
+    b=int(input("Enter your number: "))
+    c=int(input("Enter your number: "))
+    average=(a+b+c)/3
+    print(average)
+avg()
+print("thank you")
+avg()
+avg()    
